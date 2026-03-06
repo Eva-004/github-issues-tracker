eb7e0d0d6daa399a9bc8db801a5a89ff773fd857
Eva-004 <foredajannateva@gmail.com>
2026-03-07 05:19:38 +0600

    load open issues and closed issue from api

--- scripts/home.js
+++ scripts/home.js
@@ -1,7 +1,15 @@
 let allIssues = [];
+let openIssues = [];
+let closedIssues = [];
 const allBtn = document.getElementById('all-btn');
 const allIssue = document.getElementById('allIssue');
 const allIssueContainer = document.getElementById('allIssueContainer');
+const openClosedIcon = document.getElementById('openClosedIcon');
+const openIssue = document.getElementById('open-btn');
+const closedBtn = document.getElementById('closed-btn');
+const openClosAllBtn = document.querySelectorAll('.open-close-all');
+const searchBar = document.getElementById('search');
+
  async function loadAllIssues() {
     const res = await fetch('https://phi-lab-server.vercel.app/api/v1/lab/issues');
     const data = await res.json();
@@ -11,21 +19,21 @@ const allIssueContainer = document.getElementById('allIssueContainer');
     })
      allIssue.innerText=allIssues.length;
  }
- async function displayAllIssues(data) {
+ function displayAllIssues(data) {
+      allIssueContainer.innerHTML='';
     data.forEach(item=>{
+        loadSingleIssue(item.id);
         const div = document.createElement('div');
         div.className='card  bg-base-100 card-sm shadow-lg';
         div.innerHTML=`
          <div class="card-body space-y-3">
                     <div class="flex justify-between items-center">
-                        <div><i class="fa-solid fa-circle-dot text-accent text-xl"></i></div>
+                        <div id="openClosedIcon"><img src="./assets/Open-Status.png" alt=""></div>
                         <span id="status" class="py-2 px-4 bg-red-100 text-red-500 font-semibold rounded-xl text-center">${item.priority}</span>
                     </div>
                     <h2 class="font-semibold">${item.title}</h2>
                     <p class="line-clamp-2 text-[#64748B]">${item.description}</p>
-                    <div class="flex gap-3 items-center">
-                         <span id="status" class=" bg-red-100 text-red-500 font-semibold rounded-2xl text-center"><i class="fa-solid fa-bug"></i> Bug</span>
-                         <span id="status" class=" bg-yellow-100 text-yellow-500 font-semibold rounded-2xl text-center"><i class="fa-solid fa-stroopwafel"></i> Help wanted</span>
+                    <div id="allLabels" class="flex gap-3 items-center labels">
                     </div>
                     <hr class="text-[#64748B80]">
                     <div class="space-y-1">
@@ -33,12 +41,126 @@ const allIssueContainer = document.getElementById('allIssueContainer');
                         <p class="text-[#64748B]">${item.createdAt}</p>
                     </div>
                 </div>
-        `
+        ` 
         allIssueContainer.appendChild(div);
+        
     })
  }
- let totalOpenIssue=0;
- let totalCloseIssue=0;
 
+async function loadSingleIssue(id) {
+    const res = await fetch(`https://phi-lab-server.vercel.app/api/v1/lab/issue/${id}`);
+    const data = await res.json();
+   if(data.data.status === 'open'){
+     openIssues.push(data.data);
+   }else if(data.data.status === 'closed'){
+     closedIssues.push(data.data);
+   }else{
+     allIssues.push(data.data);
+   }
+}
+
+function displayOpenIssue(){
+   
+   allIssueContainer.innerHTML='';
+    allIssue.innerText=openIssues.length;
+   openIssues.forEach(item=>{
+        const div = document.createElement('div');
+        div.className='card  bg-base-100 card-sm shadow-lg';
+        div.innerHTML=`
+         <div class="card-body space-y-3">
+                    <div class="flex justify-between items-center">
+                        <div id="openClosedIcon"><img src="./assets/Open-Status.png" alt=""></div>
+                        <span id="status" class="py-2 px-4 bg-red-100 text-red-500 font-semibold rounded-xl text-center">${item.priority}</span>
+                    </div>
+                    <h2 class="font-semibold">${item.title}</h2>
+                    <p class="line-clamp-2 text-[#64748B]">${item.description}</p>
+                    <div id="allLabels" class="flex gap-3 items-center labels">
+                    </div>
+                    <hr class="text-[#64748B80]">
+                    <div class="space-y-1">
+                        <p class="text-[#64748B]">#${item.id} by ${item.author}</p>
+                        <p class="text-[#64748B]">${item.createdAt}</p>
+                    </div>
+                </div>
+        ` 
+        allIssueContainer.appendChild(div);
+        activeBtn(openIssue);
+    })
+
+}
+
+function displayClosedIssue(){
+    
+   allIssueContainer.innerHTML='';
+    allIssue.innerText=closedIssues.length;
+   closedIssues.forEach(item=>{
+        const div = document.createElement('div');
+        div.className='card  bg-base-100 card-sm shadow-lg';
+        div.innerHTML=`
+         <div class="card-body space-y-3">
+                    <div class="flex justify-between items-center">
+                        <div id="openClosedIcon"><img src="./assets/Open-Status.png" alt=""></div>
+                        <span id="status" class="py-2 px-4 bg-red-100 text-red-500 font-semibold rounded-xl text-center">${item.priority}</span>
+                    </div>
+                    <h2 class="font-semibold">${item.title}</h2>
+                    <p class="line-clamp-2 text-[#64748B]">${item.description}</p>
+                    <div id="allLabels" class="flex gap-3 items-center labels">
+                    </div>
+                    <hr class="text-[#64748B80]">
+                    <div class="space-y-1">
+                        <p class="text-[#64748B]">#${item.id} by ${item.author}</p>
+                        <p class="text-[#64748B]">${item.createdAt}</p>
+                    </div>
+                </div>
+        ` 
+        allIssueContainer.appendChild(div);
+        activeBtn(closedBtn);
+    })
+
+}
+ allBtn.addEventListener('click',()=>{
+     
+  allIssueContainer.innerHTML='';
+    allIssue.innerText=allIssues.length;
+     allIssues.forEach(item=>{
+        const div = document.createElement('div');
+        div.className='card  bg-base-100 card-sm shadow-lg';
+        div.innerHTML=`
+         <div class="card-body space-y-3">
+                    <div class="flex justify-between items-center">
+                        <div id="openClosedIcon"><img src="./assets/Open-Status.png" alt=""></div>
+                        <span id="status" class="py-2 px-4 bg-red-100 text-red-500 font-semibold rounded-xl text-center">${item.priority}</span>
+                    </div>
+                    <h2 class="font-semibold">${item.title}</h2>
+                    <p class="line-clamp-2 text-[#64748B]">${item.description}</p>
+                    <div id="allLabels" class="flex gap-3 items-center labels">
+                    </div>
+                    <hr class="text-[#64748B80]">
+                    <div class="space-y-1">
+                        <p class="text-[#64748B]">#${item.id} by ${item.author}</p>
+                        <p class="text-[#64748B]">${item.createdAt}</p>
+                    </div>
+                </div>
+        ` 
+        allIssueContainer.appendChild(div);
+        activeBtn(allBtn);
+    })
+ })
+
+ function activeBtn(id){
+     openClosAllBtn.forEach(item=>{
+       item.classList.remove('btn-primary','text-white');
+     item.classList.add('btn-outline');
+     })
+    id.classList.remove('btn-outline');
+    id.classList.add('btn-primary','text-white');
+ }
+ async function loadSearchValue() {
+    const searchText = searchBar.value;
+    searchBar.innerText='';
+    const res=await fetch(`https://phi-lab-server.vercel.app/api/v1/lab/issues/search?q=${searchText}`);
+    const data = await res.json();
+   displayAllIssues(data.data);
+ }
  loadAllIssues();
  
